578e53e22451b29e4bf333df7f9906ec7a8529fb
Artem Ustas <art4yus@ya.ru>
2017-02-08 16:06:23 +0300

    Добавил все аттрибуты в html (ранее проставлялись через js)

--- js/form.js
+++ js/form.js
@@ -5,22 +5,7 @@ var activePin = tokyoPinMap.querySelector('.pin--active');
 var dialogMain = document.querySelector('.dialog');
 var dialogClose = dialogMain.querySelector('.dialog__close');
 var noticeForm = document.querySelector('.notice__form');
-var noticeTitle = noticeForm.querySelector('#title');
 var noticePrice = noticeForm.querySelector('#price');
-var noticeAddress = noticeForm.querySelector('#address');
-var noticeHouseType = noticeForm.querySelector('#type');
-var noticeRoomNumbers = noticeForm.querySelector('#room_number');
-var noticeCapacity = noticeForm.querySelector('#capacity');
-var noticeTime = noticeForm.querySelector('#time');
-var noticeTimeOut = noticeForm.querySelector('#timeout');
-
-noticeForm.setAttribute('name', 'noticeForm');
-noticeHouseType.setAttribute('name', 'type');
-noticeRoomNumbers.setAttribute('name', 'roomNumber');
-noticeCapacity.setAttribute('name', 'capacity');
-noticeTime.setAttribute('name', 'timeIn');
-noticeTimeOut.setAttribute('name', 'timeOut');
-
 var selectedHouseType = noticeForm.elements.type;
 var selectedRoomNumbers = noticeForm.elements.roomNumber;
 var selectedCapacity = noticeForm.elements.capacity;
@@ -29,17 +14,6 @@ var selectedTimeOut = noticeForm.elements.timeOut;
 
 var ENTER_KEY_CODE = 13;
 var ESCAPE_KEY_CODE = 27;
-
-noticeTitle.required = true;
-noticeTitle.minLength = '30';
-noticeTitle.maxLength = '100';
-
-noticePrice.required = true;
-noticePrice.type = 'number';
-noticePrice.setAttribute('min', '1000');
-noticePrice.setAttribute('max', '1000000');
-
-noticeAddress.required = true;
 
 function setupARIA(element, atribute1, atribute2) {
   element.setAttribute(atribute1, atribute2);
@@ -105,7 +79,7 @@ function getPinTarget(e) {
   }
 }
 
-function isActivateEvent(e) {
+function keydownHandler(e) {
   var activateEvent = e.keyCode;
   switch (activateEvent) {
     case ENTER_KEY_CODE:
@@ -119,7 +93,7 @@ function isActivateEvent(e) {
 
 tokyoPinMap.addEventListener('click', getPinTarget);
 tokyoPinMap.addEventListener('keydown', function (e) {
-  isActivateEvent(e);
+  keydownHandler(e);
 });
 
 dialogClose.addEventListener('click', letDialogClose);
