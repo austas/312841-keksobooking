49068ab26094a4c93ec54c6c8effe804a18404ce
Artem Ustas <art4yus@ya.ru>
2017-02-09 14:32:04 +0300

    Перенес всю логику по отрисовке меток на карте(+обработчики, закрытие\открытие карточки, отметка акт. метки) в файл initialize-pins.js. Подключил его в index.html

--- js/form.js
+++ js/form.js
@@ -1,9 +1,5 @@
 'use strict';
 
-var tokyoPinMap = document.querySelector('.tokyo__pin-map');
-var activePin = tokyoPinMap.querySelector('.pin--active');
-var dialogMain = document.querySelector('.dialog');
-var dialogClose = dialogMain.querySelector('.dialog__close');
 var noticeForm = document.querySelector('.notice__form');
 var noticePrice = noticeForm.querySelector('#price');
 var selectedHouseType = noticeForm.elements.type;
@@ -11,37 +7,6 @@ var selectedRoomNumbers = noticeForm.elements.roomNumber;
 var selectedCapacity = noticeForm.elements.capacity;
 var selectedTimeIn = noticeForm.elements.timeIn;
 var selectedTimeOut = noticeForm.elements.timeOut;
-
-var ENTER_KEY_CODE = 13;
-var ESCAPE_KEY_CODE = 27;
-
-function setupARIA(element, atribute1, atribute2) {
-  element.setAttribute(atribute1, atribute2);
-}
-
-function selectActivePin(evt) {
-  deleteActivePin();
-  evt.classList.add('pin--active');
-  dialogMain.style.display = 'block';
-  setupARIA(evt, 'aria-pressed', 'true');
-  setupARIA(dialogMain, 'aria-hidden', 'false');
-  setupARIA(dialogClose, 'aria-pressed', 'false');
-}
-
-function deleteActivePin() {
-  activePin = document.querySelector('.pin--active');
-  if (activePin) {
-    activePin.classList.remove('pin--active');
-    setupARIA(activePin, 'aria-pressed', 'false');
-  }
-}
-
-function dialogCloseHandler() {
-  dialogMain.style.display = 'none';
-  setupARIA(dialogMain, 'aria-hidden', 'true');
-  setupARIA(dialogClose, 'aria-pressed', 'true');
-  deleteActivePin();
-}
 
 function syncSelectedElements(selectedOption) {
   selectedOption = selectedOption.options[selectedOption.selectedIndex].value;
@@ -68,36 +33,6 @@ function syncSelectedElements(selectedOption) {
   }
 }
 
-function pinTargetHandler(evt) {
-  var target = evt.target;
-  while (target !== tokyoPinMap) {
-    if (target.classList.contains('pin')) {
-      selectActivePin(target);
-      return;
-    }
-    target = target.parentNode;
-  }
-}
-
-function keydownHandler(evt) {
-  var activateEvent = evt.keyCode;
-  switch (activateEvent) {
-    case ENTER_KEY_CODE:
-      pinTargetHandler(evt);
-      break;
-    case ESCAPE_KEY_CODE:
-      dialogCloseHandler(evt);
-      break;
-  }
-}
-
-tokyoPinMap.addEventListener('click', pinTargetHandler);
-tokyoPinMap.addEventListener('keydown', function (evt) {
-  keydownHandler(evt);
-});
-
-dialogClose.addEventListener('click', dialogCloseHandler);
-
 selectedHouseType.addEventListener('change', function () {
   syncSelectedElements(selectedHouseType);
 });
@@ -113,3 +48,5 @@ selectedTimeIn.addEventListener('change', function (evt) {
 selectedTimeOut.addEventListener('change', function (evt) {
   selectedTimeIn.value = selectedTimeOut.value;
 });
+
+window.initializePins();
---
+++ js/initialize-pins.js
@@ -0,0 +1,71 @@
+'use strict';
+
+window.initializePins = function () {
+
+  var tokyoPinMap = document.querySelector('.tokyo__pin-map');
+  var activePin = tokyoPinMap.querySelector('.pin--active');
+  var dialogMain = document.querySelector('.dialog');
+  var dialogClose = dialogMain.querySelector('.dialog__close');
+
+  var ENTER_KEY_CODE = 13;
+  var ESCAPE_KEY_CODE = 27;
+
+  function setupARIA(element, atribute1, atribute2) {
+    element.setAttribute(atribute1, atribute2);
+  }
+
+  function selectActivePin(evt) {
+    deleteActivePin();
+    evt.classList.add('pin--active');
+    dialogMain.style.display = 'block';
+    setupARIA(evt, 'aria-pressed', 'true');
+    setupARIA(dialogMain, 'aria-hidden', 'false');
+    setupARIA(dialogClose, 'aria-pressed', 'false');
+  }
+
+  function deleteActivePin() {
+    activePin = document.querySelector('.pin--active');
+    if (activePin) {
+      activePin.classList.remove('pin--active');
+      setupARIA(activePin, 'aria-pressed', 'false');
+    }
+  }
+
+  function dialogCloseHandler() {
+    dialogMain.style.display = 'none';
+    setupARIA(dialogMain, 'aria-hidden', 'true');
+    setupARIA(dialogClose, 'aria-pressed', 'true');
+    deleteActivePin();
+  }
+
+  function pinTargetHandler(evt) {
+    var target = evt.target;
+    while (target !== tokyoPinMap) {
+      if (target.classList.contains('pin')) {
+        selectActivePin(target);
+        return;
+      }
+      target = target.parentNode;
+    }
+  }
+
+  function keydownHandler(evt) {
+    var activateEvent = evt.keyCode;
+    switch (activateEvent) {
+      case ENTER_KEY_CODE:
+        pinTargetHandler(evt);
+        break;
+      case ESCAPE_KEY_CODE:
+        dialogCloseHandler(evt);
+        break;
+    }
+  }
+
+  tokyoPinMap.addEventListener('click', pinTargetHandler);
+  tokyoPinMap.addEventListener('keydown', function (evt) {
+    keydownHandler(evt);
+  });
+
+  dialogClose.addEventListener('click', dialogCloseHandler);
+
+};
