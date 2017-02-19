f28d4f4eb8a0433fae61f4406b21fe4b7d48b092
Artem Ustas <art4yus@ya.ru>
2017-02-20 01:08:34 +0300

    Переписал логику синх полей, через функции обратного вызова

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -3,6 +3,6 @@
 window.synchronizeFields = (function () {
   return function (element1, element2, array1, array2, value) {
     var selectedOption = element1.options[element1.selectedIndex].value;
-    element2[value] = array2[array1.indexOf(selectedOption)];
+    value(element2, array2[array1.indexOf(selectedOption)]);
   };
 })();
